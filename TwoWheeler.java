377cdfd834fd7c58c70e883e6dd7a552304e76db
RamkumarS95 <124465852+RamkumarS95@users.noreply.github.com>
2023-02-05 01:37:02 +0530

    Add files via upload

---
+++ TwoWheeler.java
@@ -0,0 +1,19 @@
+package week1.day1;
+
+public class TwoWheeler {
+	public static void main(String[] args) {
+		int noOfWheels =2;
+		short noOfMirrors=4;
+		long chassisNumber=678767656547879098l;
+		boolean isPunctured=true;
+		String bikeName="Honda";
+		double runningKM=60;
+		System.out.println(noOfWheels);
+		System.out.println(noOfMirrors);
+		System.out.println(chassisNumber);
+		System.out.println(isPunctured);
+		System.out.println(bikeName);
+		System.out.println(runningKM);
+	}
+
+}
